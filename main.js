96221a6092451a4f071e378c7adaa8388cdd51b2
vivihelo <134708030+vivihelo@users.noreply.github.com>
2024-06-19 10:50:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector("caixa-pergunta");
+const caixaAlternativa = document.querySelector("caixa-alternativa");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
+
+const perguntas = [
+
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alterativa 1",
+            "Alternativa 2"
+        ]
+    },
+
+];
+
+let atual = 0;
+let perguntaAtual
+
+function mostraPergunta () {
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent
+
+}
